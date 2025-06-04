a45e233f430b28e4d6da64e9ba865776d20d47ec
Pierre THIEBERT <github@thiebert.me>
2025-06-04 10:47:24 +0200

    remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./App"; // Nouveau fichier App.tsx
+import { App } from "./App";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
