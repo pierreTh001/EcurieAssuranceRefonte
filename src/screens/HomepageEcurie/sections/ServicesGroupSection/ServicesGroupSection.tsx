191428b2837d330c0d91e4399f460679ef79c762
Pierre THIEBERT <github@thiebert.me>
2025-06-04 14:03:58 +0200

    set bandeau liens

--- src/screens/HomepageEcurie/sections/ServicesGroupSection/ServicesGroupSection.tsx
+++ src/screens/HomepageEcurie/sections/ServicesGroupSection/ServicesGroupSection.tsx
@@ -17,7 +17,7 @@ export const ServicesGroupSection = (): JSX.Element => {
         {/* Background decorative elements */}
         <div className="absolute w-full h-[50px] md:h-[81px] top-[60px] md:top-[59px] left-0 bg-[#2ef3c5] rotate-[180deg] bandeauBack" />
 
-        <div className="absolute w-full h-[50px] md:h-[81px] top-[24px] md:top-7 left-0 bg-[#233c37] rotate-2">
+        <div className="absolute w-full h-[50px] md:h-[81px] top-[24px] md:top-7 left-0 bg-[#233c37] rotate-2 beandeauParent">
           {/* Services list */}
           <div className="flex flex-wrap justify-center md:justify-start items-start gap-4 sm:gap-6 md:gap-8 xl:gap-10 2xl:gap-[65px] relative top-2 md:top-4 md:left-[62px] px-4 md:px-0 bandeauFront">
             {services.map((service, index) => (
